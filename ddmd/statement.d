8dfa7b3a2f118af4520f014b319e351842919f1b
Johan Engelen <jbc.engelen@gmail.com>
2016-03-25 15:44:05 +0100

    [Weka] Remove the "statement is not reachable" warning.

--- ddmd/statement.d
+++ ddmd/statement.d
@@ -343,7 +343,7 @@ public:
                         }
                         if (!(result & BEfallthru) && !s.comeFrom())
                         {
-                            if (s.blockExit(func, mustNotThrow) != BEhalt && s.hasCode())
+                            if (!IN_WEKA && s.blockExit(func, mustNotThrow) != BEhalt && s.hasCode())
                                 s.warning("statement is not reachable");
                         }
                         else
