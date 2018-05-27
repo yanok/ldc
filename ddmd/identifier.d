16ef71f72c8cc164ed00b1033bfc3299a7eccc7d
Johan Engelen <jbc.engelen@gmail.com>
2018-05-27 11:46:09 +0200

    Backport https://github.com/dlang/dmd/pull/8255 to fix WEKAPP-65659

--- ddmd/identifier.d
+++ ddmd/identifier.d
@@ -155,6 +155,14 @@ public:
         buf.printf("%llu", cast(ulong)loc.linnum);
         buf.writestring("_C");
         buf.printf("%llu", cast(ulong)loc.charnum);
+        auto basesize = buf.peekSlice().length;
+        uint counter = 1;
+        while (stringtable.lookup(buf.peekSlice().ptr, buf.peekSlice().length) !is null)
+        {
+            buf.setsize(basesize);
+            buf.writestring("_");
+            buf.printf("%u", counter++);
+        }
         return idPool(buf.peekSlice());
     }
 
