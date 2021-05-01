e008caad48097785a2c4ee0860f8d4601c30b1d6
Eyal Lotem <eyal@weka.io>
2021-05-02 01:17:33 +0300

    A type has a unique id for fast ptr-like comparison

--- dmd/mtype.d
+++ dmd/mtype.d
@@ -387,6 +387,8 @@ extern (C++) abstract class Type : ASTNode
     TY ty;
     MOD mod; // modifiers MODxxxx
     char* deco;
+    alias UniqueId = ulong;
+    UniqueId uniqueId;             // incremented by 1 in each construction
 
     static struct Mcache
     {
@@ -510,9 +512,12 @@ version (IN_LLVM)
             return sizeTy;
         }();
 
+    static __gshared ulong nextUniqueId = 0x100000;
+
     final extern (D) this(TY ty)
     {
         this.ty = ty;
+        this.uniqueId = nextUniqueId++;
     }
 
     const(char)* kind() const nothrow pure @nogc @safe
@@ -524,6 +529,7 @@ version (IN_LLVM)
     {
         Type t = cast(Type)mem.xmalloc(sizeTy[ty]);
         memcpy(cast(void*)t, cast(void*)this, sizeTy[ty]);
+        t.uniqueId = nextUniqueId++;
         return t;
     }
 
@@ -1156,6 +1162,7 @@ version (IN_LLVM)
             (cast(TypeStruct)t).att = AliasThisRec.fwdref;
         if (t.ty == Tclass)
             (cast(TypeClass)t).att = AliasThisRec.fwdref;
+        t.uniqueId = nextUniqueId++;
         return t;
     }
 
