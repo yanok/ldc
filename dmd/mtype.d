5d6afcbf99c706eed00bf34a9f0537db9f55296f
Johan Engelen <jbc.engelen@gmail.com>
2021-08-15 15:19:51 +0200

    wekamod: Disable the change by https://github.com/dlang/dmd/pull/12090 to fix bug https://issues.dlang.org/show_bug.cgi?id=22186 .

--- dmd/mtype.d
+++ dmd/mtype.d
@@ -5094,6 +5094,7 @@ extern (C++) final class TypeFunction : TypeNext
     /** Extends TypeNext.constConv by also checking for matching attributes **/
     override MATCH constConv(Type to)
     {
+        if (!IN_WEKA())
         // Attributes need to match exactly, otherwise it's an implicit conversion
         if (this.ty != to.ty || !this.attributesEqual(cast(TypeFunction) to))
             return MATCH.nomatch;
