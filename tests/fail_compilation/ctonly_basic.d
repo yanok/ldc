06264b701379a3eb2491c9fd4752be58be4c67d1
Ilya Yanok <ilya.yanok@weka.io>
2025-03-11 17:27:30 +0100

    ctonly: tests for basic (non-inference) cases

---
+++ tests/fail_compilation/ctonly_basic.d
@@ -0,0 +1,24 @@
+/* TEST_OUTPUT:
+---
+fail_compilation/ctonly_basic.d(23): Error: cannot call @__ctfe function `ctonly_basic.f` from non-@__ctfe function `D main`
+fail_compilation/ctonly_basic.d(23): Error: cannot call @__ctfe function `ctonly_basic.g!2.g` from non-@__ctfe function `D main`
+fail_compilation/ctonly_basic.d(23): Error: cannot take address of @__ctfe function `f`
+fail_compilation/ctonly_basic.d(23): Error: cannot take address of @__ctfe function `g`
+---
+*/
+
+int f(int x, int y) @__ctfe {
+    return x + y;
+}
+
+int g(int x)(int y) @__ctfe {
+    return x + y;
+}
+
+alias gf = g!2;
+
+import std.stdio;
+
+void main() {
+    writeln(f(2, 4), gf(0), &f, &gf);
+}
---
+++ tests/fail_compilation/ctonly_class.d
@@ -0,0 +1,28 @@
+/* TEST_OUTPUT:
+---
+fail_compilation/ctonly_class.d(24): Error: cannot call @__ctfe function `ctonly_class.C.f` from non-@__ctfe function `D main`
+fail_compilation/ctonly_class.d(25): Error: cannot call @__ctfe function `ctonly_class.C.g` from non-@__ctfe function `D main`
+fail_compilation/ctonly_class.d(27): Error: cannot call @__ctfe function `ctonly_class.CtOnly.this` from non-@__ctfe function `D main`
+---
+*/
+class C {
+    int v;
+    this(int v_) { v = v_; }
+    int f(int x) const @__ctfe { return x + v; }
+    static int g(int x) @__ctfe { return x + 2; }
+}
+
+class CtOnly {
+    int v;
+    this(int v_) @__ctfe { v = v_; }
+    int f(int x) const { return x + v; }
+    static int g(int x) { return x + 2; }
+}
+
+void main() {
+    C i = new C(1);
+    int a = i.f(3);
+    int v = C.g(2);
+
+    auto cl = new CtOnly(5);
+}
