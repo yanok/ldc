b389c17b4986f78e04b7a4bc615c8e44039f50ac
Ilya Yanok <ilya.yanok@weka.io>
2025-03-11 17:28:09 +0100

    ctonly: tests for inference

---
+++ tests/compilable/ctonly_templates.d
@@ -0,0 +1,15 @@
+import std.algorithm.iteration;
+import std.array;
+
+int f(int x) @__ctfe
+{
+    return x + 1;
+}
+
+enum a = map!f([1, 2, 3]).array;
+
+import std.stdio;
+
+void main() {
+    writeln(a);
+}
---
+++ tests/fail_compilation/ctonly_templates.d
@@ -0,0 +1,22 @@
+/* TEST_OUTPUT:
+---
+fail_compilation/ctonly_templates.d(21): Error: cannot call @__ctfe function `std.array.array!(MapResult!(f, int[])).array` from non-@__ctfe function `D main`
+fail_compilation/ctonly_templates.d(21):        `std.array.array!(MapResult!(f, int[])).array` was inferred to be @__ctfe because it (transitively) calls `ctonly_templates.f`
+---
+*/
+import std.algorithm.iteration;
+import std.array;
+
+int f(int x) @__ctfe
+{
+    return x + 1;
+}
+
+enum a = map!f([1, 2, 3]);
+
+import std.stdio;
+
+void main() {
+    // That fails, because `f` is called internally.
+    writeln(a.array);
+}
