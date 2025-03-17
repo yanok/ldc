0cd92852f9be0b0bbdd7f6610588959e287d7635
Ilya Yanok <ilya.yanok@weka.io>
2025-03-17 16:40:57 +0100

    Fix check lines for template tests

--- tests/fail_compilation/ctonly_templates.d
+++ tests/fail_compilation/ctonly_templates.d
@@ -1,11 +1,5 @@
 // RUN: not %ldc -c %s 2>&1 | FileCheck %s
 
-/* TEST_OUTPUT:
----
-fail_compilation/ctonly_templates.d(23): Error: cannot call @__ctfe function `std.array.array!(MapResult!(f, int[])).array` from non-@__ctfe function `D main`
-fail_compilation/ctonly_templates.d(23):        `std.array.array!(MapResult!(f, int[])).array` was inferred to be @__ctfe because it (transitively) calls `ctonly_templates.f`
----
-*/
 import std.algorithm.iteration;
 import std.array;
 
@@ -20,5 +14,6 @@ import std.stdio;
 
 void main() {
     // That fails, because `f` is called internally.
+    // CHECK: ctonly_templates.d([[@LINE+1]]): Error: cannot call @__ctfe function `std.array.array!(MapResult!(f, int[])).array` from non-@__ctfe function `D main`
     writeln(a.array);
 }
