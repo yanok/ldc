43ad824145512fc4c40a9bf6a98f212aa0118486
Ilya Yanok <ilya.yanok@weka.io>
2025-03-17 16:41:27 +0100

    Fix CHECK lines for basic tests

--- tests/fail_compilation/ctonly_basic.d
+++ tests/fail_compilation/ctonly_basic.d
@@ -1,14 +1,5 @@
 // RUN: not %ldc -c %s 2>&1 | FileCheck %s
 
-/* TEST_OUTPUT:
----
-fail_compilation/ctonly_basic.d(25): Error: cannot call @__ctfe function `ctonly_basic.f` from non-@__ctfe function `D main`
-fail_compilation/ctonly_basic.d(25): Error: cannot call @__ctfe function `ctonly_basic.g!2.g` from non-@__ctfe function `D main`
-fail_compilation/ctonly_basic.d(25): Error: cannot take address of @__ctfe function `f`
-fail_compilation/ctonly_basic.d(25): Error: cannot take address of @__ctfe function `g`
----
-*/
-
 int f(int x, int y) @__ctfe {
     return x + y;
 }
@@ -22,5 +13,9 @@ alias gf = g!2;
 import std.stdio;
 
 void main() {
+    // CHECK: ctonly_basic.d([[@LINE+4]]): Error: cannot call @__ctfe function `ctonly_basic.f` from non-@__ctfe function `D main`
+    // CHECK: ctonly_basic.d([[@LINE+3]]): Error: cannot call @__ctfe function `ctonly_basic.g!2.g` from non-@__ctfe function `D main`
+    // CHECK: ctonly_basic.d([[@LINE+2]]): Error: cannot take address of CTFE-only function `f`
+    // CHECK: ctonly_basic.d([[@LINE+1]]): Error: cannot take address of CTFE-only function `g`
     writeln(f(2, 4), gf(0), &f, &gf);
 }
--- tests/fail_compilation/ctonly_class.d
+++ tests/fail_compilation/ctonly_class.d
@@ -1,12 +1,5 @@
 // RUN: not %ldc -c %s 2>&1 | FileCheck %s
 
-/* TEST_OUTPUT:
----
-fail_compilation/ctonly_class.d(26): Error: cannot call @__ctfe function `ctonly_class.C.f` from non-@__ctfe function `D main`
-fail_compilation/ctonly_class.d(27): Error: cannot call @__ctfe function `ctonly_class.C.g` from non-@__ctfe function `D main`
-fail_compilation/ctonly_class.d(29): Error: cannot call @__ctfe function `ctonly_class.CtOnly.this` from non-@__ctfe function `D main`
----
-*/
 class C {
     int v;
     this(int v_) { v = v_; }
@@ -23,8 +16,11 @@ class CtOnly {
 
 void main() {
     C i = new C(1);
+    // CHECK: ctonly_class.d([[@LINE+1]]): Error: cannot call @__ctfe function `ctonly_class.C.f` from non-@__ctfe function `D main`
     int a = i.f(3);
+    // CHECK: ctonly_class.d([[@LINE+1]]): Error: cannot call @__ctfe function `ctonly_class.C.g` from non-@__ctfe function `D main`
     int v = C.g(2);
 
+    // CHECK: ctonly_class.d([[@LINE+1]]): Error: cannot call @__ctfe function `ctonly_class.CtOnly.this` from non-@__ctfe function `D main`
     auto cl = new CtOnly(5);
 }
