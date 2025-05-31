a663138af789bbe3922b3a6085a56981af041abf
Ilya Yanok <ilya.yanok@weka.io>
2025-05-31 12:25:47 +0200

    ldc: add new -Honly flag
    To only generate headers and quit

--- dmd/globals.h
+++ dmd/globals.h
@@ -219,6 +219,7 @@ struct Param
     Array<const char *> ddocfiles;  // macro include files for Ddoc
 
     bool doHdrGeneration;  // process embedded documentation comments
+    bool doHdrGenerationOnly; // generate 'header' file and exit
     DString hdrdir;        // write 'header' file to docdir directory
     DString hdrname;       // write 'header' file to docname
     bool hdrStripPlainFunctions; // strip the bodies of plain (non-template) functions
